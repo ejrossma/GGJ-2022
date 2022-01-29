57201623f42d14acc8bc8669b7b6466836be5339
ejrossma <ejrossma@ucsc.edu>
2022-01-28 18:42:53 -0800

    Further work on enemy

--- GGJ 2022/Assets/Scripts/Enemy.cs
+++ GGJ 2022/Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     //larger size = more hp & less speed
     //lesser size = less hp & more speed
     public int hp;
-    private float size;
+    public float size;
     public int enemySpeed;
 
     // Update is called once per frame
--- GGJ 2022/Assets/Scripts/Enemy_Spawner.cs
+++ GGJ 2022/Assets/Scripts/Enemy_Spawner.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Enemy_Spawner : MonoBehaviour
 {
     public GameObject enemy;
-    
     private Transform enemyContainer;
 
+    private GameManager gm;
+
+    public int baseHealth;
+    public int baseSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyContainer = transform.GetChild(0);
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -24,7 +28,9 @@ public class Enemy_Spawner : MonoBehaviour
     public void generateEnemies() {
         //choose size
         float enemySize = Random.Range(0.75f, 1.25f);
+
         //get health and speed based on size + time into game
-        
+        int enemyHealth = (int) (enemySize * baseHealth * gm.difficulty);
+        int enemySpeed = (int) ((baseSpeed / (int) enemySize) * gm.difficulty);
     }
 }
