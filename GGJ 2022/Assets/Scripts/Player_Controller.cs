8f039c1d73c381054eda94b66d01886bad52e53c
Adrian <adrianlance86@gmail.com>
2022-01-28 18:26:16 -0800

    updated to rigid body movement

--- GGJ 2022/Assets/Scripts/Player_Controller.cs
+++ GGJ 2022/Assets/Scripts/Player_Controller.cs
@@ -8,16 +8,23 @@ public class Player_Controller : MonoBehaviour
     private float inputX;
     private float inputY;
     private Vector2 inputVector;
+    private Rigidbody2D playerRB;
+    private void Start() {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
     private void Update() {
         PlayerInput();
+    }
+    private void FixedUpdate() {
         Movement();
     }
     private void PlayerInput(){
         inputX = Input.GetAxisRaw("Horizontal");
         inputY = Input.GetAxisRaw("Vertical");
-        inputVector = new Vector2(inputX, inputY).normalized * Time.deltaTime * playerSpeed;
+        inputVector = new Vector2(inputX, inputY).normalized;
     }
     private void Movement(){
-        transform.position = new Vector2(transform.position.x + inputVector.x, transform.position.y + inputVector.y);
+        //transform.position = new Vector2(transform.position.x + inputVector.x, transform.position.y + inputVector.y);
+        playerRB.velocity = inputVector * Time.deltaTime * playerSpeed;
     }
 }
