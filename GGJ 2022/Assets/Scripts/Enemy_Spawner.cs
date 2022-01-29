5114f6515fabf3958f2f99a8de36438d4ae397e0
ejrossma <ejrossma@ucsc.edu>
2022-01-28 22:12:53 -0800

    Got Enemy_Spawner Working Again

--- GGJ 2022/Assets/Scripts/Enemy_Spawner.cs
+++ GGJ 2022/Assets/Scripts/Enemy_Spawner.cs
@@ -5,30 +5,29 @@ using UnityEngine;
 public class Enemy_Spawner : MonoBehaviour
 {
     public GameObject enemy;
-    private Transform enemyContainer;
 
-    private GameManager gm;
+    public GameManager gm;
 
-    public int baseHealth = 10;
-    public int baseSpeed = 5;
+    public float baseHealth = 10;
+    public float baseSpeed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        gm = FindObjectOfType<GameManager>();
+
     }
 
     public void generateEnemy(Vector3 position) {
         //choose size
         float enemySize = Random.Range(0.75f, 1.25f);
         //get health and speed based on size + time into game
-        int enemyHealth = (int) (enemySize * baseHealth * gm.difficulty);
-        int enemySpeed = (int) ((baseSpeed / enemySize) * gm.difficulty);
+        float enemyHealth = enemySize * baseHealth * gm.difficulty;
+        float enemySpeed = (baseSpeed / enemySize) * gm.difficulty;
 
         GameObject spawnedEnemy = Instantiate(enemy, position, Quaternion.identity);
         spawnedEnemy.transform.parent = transform;
-        spawnedEnemy.GetComponent<Enemy>().hp = enemyHealth;
-        spawnedEnemy.GetComponent<Enemy>().speed = enemySpeed;
+        spawnedEnemy.GetComponent<Enemy>().hp = (int) enemyHealth;
+        spawnedEnemy.GetComponent<Enemy>().speed = (int) enemySpeed;
         spawnedEnemy.transform.localScale = new Vector3(enemySize, enemySize, enemySize);
     }
 }
