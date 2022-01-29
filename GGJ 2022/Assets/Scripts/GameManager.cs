392d3e6b45ca8a32af744ede0ce0b014a0998e50
ejrossma <ejrossma@ucsc.edu>
2022-01-28 21:38:25 -0800

    Changed Prefab to Enemy

--- GGJ 2022/Assets/Scripts/GameManager.cs
+++ GGJ 2022/Assets/Scripts/GameManager.cs
@@ -6,18 +6,17 @@ public class GameManager : MonoBehaviour
 {
 
     public float difficulty; //base difficulty is 1
-    public Enemy_Spawner es;
+    private Enemy_Spawner es;
 
     // Start is called before the first frame update
     void Start()
     {
         es = GameObject.Find("Enemies").GetComponent<Enemy_Spawner>();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 20; i+=2)
         {
-            es.generateEnemy(new Vector3(10.0f * i, 0.0f, 0.0f));
+            es.generateEnemy(new Vector3(-10.0f + i, 0.0f, 0.0f));
         }
-        
     }
 
     // Update is called once per frame
